2cf052434c0cd9d704b0fd234eb81bf07736f9e1
RIN <hamerboy37@gmail.com>
2026-04-09 08:55:02 +0700

    Initial Commit

---
+++ LabWeek7_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LabWeek7_SwiftUI
+//
+//  Created by student on 09/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LabWeek7_SwiftUI/LabWeek7_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LabWeek7_SwiftUIApp.swift
+//  LabWeek7_SwiftUI
+//
+//  Created by student on 09/04/26.
+//
+
+import SwiftUI
+
+@main
+struct LabWeek7_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
